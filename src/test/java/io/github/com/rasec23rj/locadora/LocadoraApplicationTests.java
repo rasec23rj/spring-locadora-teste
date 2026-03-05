e54b766ccb694b817a1372617edf4ea889b2db6c
jcbarcelos <jcbarcelosrj@gmail.com>
2026-03-05 11:40:22 -0300

    inital project

---
+++ src/test/java/io/github/com/rasec23rj/locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.rasec23rj.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
